1977f3614fae73327414c10ced29d62f24ac6aa2
Diniz <barrosjunior26@outlook.com.br>
2022-07-25 13:27:59 -0300

    adicionado um laço de repetição para que o usuário possa realizar uma nova consulta

--- diaDaMulher/diaDaMulher/Program.cs
+++ diaDaMulher/diaDaMulher/Program.cs
@@ -14,30 +14,36 @@
             //Declaração de variáveis
             int opcao = 0;
 
-            //Solicitando ao usuário seu nome, sexo e valor das compras
-            Console.Write("Digite seu nome e sobrenome: ");
-            String nome = Console.ReadLine().ToUpper();
-            Console.Write("Por favor, informe seu sexo, sendo (1) para feminino ou (2) para masculino: ");
-            byte sexo = byte.Parse(Console.ReadLine());
-            Console.Write("Informe o valor total de suas compras: R$");
-            double compras = double.Parse(Console.ReadLine());
+            do
+            {
+                //Solicitando ao usuário seu nome, sexo e valor das compras
+                Console.Write("Digite seu nome e sobrenome: ");
+                String nome = Console.ReadLine().ToUpper();
+                Console.Write("Por favor, informe seu sexo, sendo (1) para feminino ou (2) para masculino: ");
+                byte sexo = byte.Parse(Console.ReadLine());
+                Console.Write("Informe o valor total de suas compras: R$");
+                double compras = double.Parse(Console.ReadLine());
 
-            //PROCESSAMENTO DOS DADOS - Condições de descontos para diferentes sexos
-            if(sexo == 1)
-            {
-                //Calculo de descontos para compras realizadas pelo sexo feminio
-                double comprasF = (compras * 13) / 100;
-                Console.WriteLine("Nome do cliente: " + nome + "\nSexo: Feminino" + "\nTotal de descontos: R$" + Math.Round(comprasF, 2) + "\nValor total das compras: R$" + Math.Round(compras, 2) + "\nValor total a pagar com desconto de 13%. R$" + Math.Round((compras - comprasF), 2));
-            }else if(sexo == 2)
-            {
-                //Calculo de descontos para compras realizadas pelo sexo feminio
-                double comprasM = (compras * 5) / 100;
-                Console.WriteLine("Nome do cliente: " + nome + "\nSexo: Masculino" + "\nTotal de descontos: R$" + Math.Round(comprasM, 2) + "\nValor total das compras: R$" + Math.Round(compras, 2) + "\nValor total a pagar com desconto de 5%. R$" + Math.Round((compras - comprasM), 2));
-            }
-            else
-            {
-                Console.WriteLine("Entrada inválida!");
-            }
+                //PROCESSAMENTO DOS DADOS - Condições de descontos para diferentes sexos
+                if (sexo == 1)
+                {
+                    //Calculo de descontos para compras realizadas pelo sexo feminio
+                    double comprasF = (compras * 13) / 100;
+                    Console.WriteLine("Nome do cliente: " + nome + "\nSexo: Feminino" + "\nTotal de descontos: R$" + Math.Round(comprasF, 2) + "\nValor total das compras: R$" + Math.Round(compras, 2) + "\nValor total a pagar com desconto de 13%. R$" + Math.Round((compras - comprasF), 2));
+                }
+                else if (sexo == 2)
+                {
+                    //Calculo de descontos para compras realizadas pelo sexo feminio
+                    double comprasM = (compras * 5) / 100;
+                    Console.WriteLine("Nome do cliente: " + nome + "\nSexo: Masculino" + "\nTotal de descontos: R$" + Math.Round(comprasM, 2) + "\nValor total das compras: R$" + Math.Round(compras, 2) + "\nValor total a pagar com desconto de 5%. R$" + Math.Round((compras - comprasM), 2));
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida!");
+                }
+                Console.Write("Digite 1 - NOVA OPERAÇÃO ou 0 - PARA SAIR: ");
+                opcao = int.Parse(Console.ReadLine());
+            } while(opcao != 0);
         }
     }
 }
